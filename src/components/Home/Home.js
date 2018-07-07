ea00aee9051f1c1e85e04b26a919495ecef7548d
Huppeli <mika.manninen93@gmail.com>
2018-07-07 23:23:17 +0300

    added splash screen and title, also semantic ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import styles from "./Home.css";
+import bg_image from '../../images/laptop_splash.jpg';
+import { Container, Header, Responsive } from 'semantic-ui-react';
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+  <Responsive style={{
+    height: "100vh",
+    background: `url(${bg_image}) no-repeat center center fixed`,
+    backgroundSize: "cover",
+    backgroundAttachment: "scroll",
+    minHeight: "100vh"
+  }}>    
+    <Container text style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      alignContent: "center",
+      height: "100%",
+      position: "relative",
+      flexWrap: "wrap"
+    }}>
+      <Header
+        as='h1'
+        content='Moi, olen Mika'
+        inverted
+        style={{
+          fontSize: "4em"
+        }}
+        />
+    </Container>    
+  </Responsive>
 )
 
 export default Home;
