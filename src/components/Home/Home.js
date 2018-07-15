dd92253d1794c85de6d4a2d579c9ef514a4682b6
Huppeli <mika.manninen93@gmail.com>
2018-07-15 16:20:10 +0300

    project data comes from array and projects are rendered based on how many there are in an array

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,52 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from "./Home.css";
 import bg_image from '../../images/laptop_splash.jpg';
 import bg_image2 from '../../images/sketch.jpg';
-import { Container, Header, Responsive } from 'semantic-ui-react';
+import { Container, Header, Responsive, Grid } from 'semantic-ui-react';
 import ProjectView from '../ProjectView/ProjectView';
 
-const Home = () => (
-  <div>
-    <Responsive style={{
-      height: "100vh",
-      background: `url(${bg_image}) no-repeat center center fixed`,
-      backgroundSize: "cover",
-      backgroundAttachment: "scroll",
-      minHeight: "100vh"
-    }}>    
-      <Container text style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-        height: "100%",
-        position: "relative",
-        flexWrap: "wrap"
-      }}>
-        <Header
-          as='h1'
-          content=""
-          inverted
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {id: 0, projectName: "Poggers" , projectColor: "orange", projectType: ["react"]},
+        {id: 1, projectName: "Nammers" , projectColor: "green", projectType: ["android"]},
+        {id: 2, projectName: "Anotha one" , projectColor: "blue", projectType: ["gitlab"]},
+        {id: 3, projectName: "Anotha one" , projectColor: "blue", projectType: ["gitlab","react"]},
+      ]
+    }
+  }
+  render() {
+    return(
+      <div>
+       <Responsive style={{
+        height: "100vh",
+        background: `url(${bg_image}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+        backgroundAttachment: "scroll",
+        minHeight: "100vh"
+        }}>    
+          <Container text style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            height: "100%",
+            position: "relative",
+            flexWrap: "wrap"
+          }}>
+           <Header
+            as='h1'
+            content=""
+            inverted
+            style={{
+              fontSize: "4em"
+            }}
+            />
+            <Header
+              as='h2'
+              content=""
+              inverted
+            />
+          </Container>    
+        </Responsive>
+        <Responsive
           style={{
-            fontSize: "4em"
+            
+            minHeight: '100vh'
           }}
-          />
-        <Header
-          as='h2'
-          content=""
-          inverted
-        />
-      </Container>    
-    </Responsive>
-    <Responsive
-      style={{
-        
-        minHeight: '100vh'
-      }}
-    >
-      <ProjectView projectName="Poggers" projectColor="orange" projectType="react" />
-    </Responsive>
-  </div>
-)
+        >
+        <Grid>
+          <Grid.Row columns={this.state.projects.length}>
+            {this.state.projects.map((project, index) => {
+            return (
+              
+              <Grid.Column key={index}>
+              <ProjectView projectName={project.projectName} projectColor={project.projectColor} projectType={project.projectType} /> 
+              </Grid.Column>
+              
+              );
+            })}
+          </Grid.Row>
+        </Grid>
+        </Responsive>
+      </div>
+    );
+  }
+}
+
 
 export default Home;
--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -4,12 +4,14 @@ import {Card, Icon } from 'semantic-ui-react';
 class ProjectView extends Component {
     render() {
         return(
-            <Card color={this.props.projectColor}> 
+            <Card fluid color={this.props.projectColor}> 
                 <Card.Content>
                     <Card.Header>{this.props.projectName}</Card.Header>
                 </Card.Content>
                 <Card.Content extra>
-                    <Icon name={this.props.projectType} />
+                    {this.props.projectType.map((type,index) => 
+                        <Icon name={type} key={index} />   
+                    )}                    
                 </Card.Content>
             </Card>
         );
